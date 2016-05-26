f7d18b35633c5e8768d693c87b38a4f276f6dfd1
Zabich <tommytruong45@gmail.com>
2016-05-26 15:55:31 -0400

    Add files via upload

---
+++ sketch_may26b/sketch_may26b.ino
@@ -0,0 +1,19 @@
+
+int ledPin = 9;
+int pirPin = 6;
+int val = 0;
+
+void setup() 
+{
+  pinMode (ledPin,OUTPUT);
+  pinMode (pirPin, INPUT);
+}
+void loop () 
+{
+  val = digitalRead(pirPin);
+  digitalWrite(ledPin,val);
+
+if (val == 1)
+  digitalWrite(ledPin,LOW);
+}
+
